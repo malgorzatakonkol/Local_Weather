c77f2e8c44521d77d70d3c520d30843201542259
Małgorzata Konkol <kalafar.malgorzata@gmail.com>
2018-06-21 20:04:07 +0200

    work with api

--- js/script.js
+++ js/script.js
@@ -10,9 +10,38 @@ $(function () {
             datatype: 'json'
         }).done(function (response) {
             console.log(response);
+            showWeather(response);
         }).fail(function (error) {
             console.log(error);
         })
     }
     loadWeather();
+
+
+    function showWeather(element) {
+        var nameCity = element.name;
+        var temperature = ((element.main.temp) - 273.15).toFixed(2);
+        var minTemp = ((element.main.temp_min) - 273.15).toFixed(2);
+        var maxTemp = ((element.main.temp_max) - 273.15).toFixed(2);
+        var weatherDescription = element.weather[0].description;
+        var weatherIcon = element.weather[0].icon;
+
+
+        console.log(nameCity);
+        console.log(temperature);
+        console.log(minTemp);
+        console.log(maxTemp);
+        console.log(weatherDescription);
+        console.log(weatherIcon);
+
+    }
+    // var button = $('#show_city');
+    // var firstInput = $('.get_name');
+    //
+    // function addInfo(e) {
+    //     e.preventDefault();
+    //     var newCity = {
+    //         name:
+    //     }
+    // }
 });
