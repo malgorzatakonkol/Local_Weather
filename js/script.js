4588047951839f1c031424f02a63884d4a071f26
Małgorzata Konkol <kalafar.malgorzata@gmail.com>
2018-06-22 20:36:44 +0200

    work with api

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(function () {
     console.log("DOM");
 
     var url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=0792ae5427c864ed05425224de36e150';
-    var ulElement = $(".description");
+    var ulElement = $(".des");
 
     var button = $('#show_city');
     var firstInput = $('.get_name');
